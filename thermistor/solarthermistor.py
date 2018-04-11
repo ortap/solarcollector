75a3c258487c51f3d4d389f1c2e143fa895f3c94
m4rz910 <m4rz910@github.com>
2018-04-11 16:34:35 -0500

    secondcommit with three sensors

--- thermistor/solarthermistor.py
+++ thermistor/solarthermistor.py
@@ -32,32 +32,47 @@ GAIN = 1
 bit_to_volt = 4.096/(pow(2,15))  #Conversion factor from bits to voltage - We are only using 15 of the 16 bits of the ADC  because the first bit is a sign bit (two's complement)
 
 #-------------------THERMISTOR SETUP------------------
-# NTC Thermister: 3kOhm, Tolerance 1%. Vishay Part Number: 01C3001FP
+# NTC Thermistor: 3kOhm, Tolerance 1%. Vishay Part Number: 01C3001FP
 #Temp Range: -40 - 150 oC
+therm_res = 3*pow(10,3) #Ohms - 3kOhm Thermistor resistance
+
 #Steinhart & hart Coefficients
 A = 1.4028*pow(10,-3)
 B = 2.3734*pow(10,-4)
 C = 9.9257*pow(10,-8)
 
+#Reference Resistor
+therm_supplyvoltage = 5 #5V supply voltage accross the thermistor and reference resistor applied
+ref_res = 1*pow(10,3)  #Ohms - 1kOhm Reference Resistance used in voltage divider calc
+
 def res_to_temp(R):
 	global A,B,C
-	T = (A + B*log(R) + C*(log(R))^3)^-1 #Steinhart Equation
-	if (T > 125) or (T < -40):
+	T = pow( A + B*log(R) + C*pow(log(R),3), -1) - 273  #Steinhart Equation and conversion from K to degrees C
+	if T > 125 or T < -40:
 		print('Warning: Thermistor Temperature Reading Out of Bounds!')
 	return T
 
 
 def vol_to_res(ADCvolt):
+	global therm_res, ref_res, therm_supplyvoltage
+	therm_res = ref_res*(therm_supplyvoltage/ADCvolt -1) #Voltage divider calculation
+	#print("Therm_Res: " + str(therm_res))
+	return therm_res
 
+def bits_to_temp(rawbit):
+	#convert from bits to voltage
+	ADCvolt = rawbit*bit_to_volt
+	#print("Ref_volt " + str(ADCvolt))
+	return res_to_temp(vol_to_res(ADCvolt))
 
-	return res
-
-
+temps = [0]*4
+values = [0]*4
 # Main loop.
 while True:
-    values = [0]*4
-    for i in range(2):
+    for i in range(4):
     	values[i] = adc.read_adc(i, gain=GAIN)
-    print(str(values[0]*bit_to_volt)+' oC')
+	temps[i]= round(bits_to_temp(values[i]),2)
+
+    print("A0: " + str(temps[0]) + " A1: " + str(temps[1]) + " A2: " + str(temps[2]) + " oC")
 
     time.sleep(0.5)
