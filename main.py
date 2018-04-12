0b5c67c9c44b7f82c21ab8ac171232bda6eb1e54
Zach <tzavelisz@gmail.com>
2018-04-11 20:15:17 -0400

    'First Thermocouple Data Collector and Main Function'

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 
-import Adafruit_ADS1x15
+from Adafruit_Python_ADS1x15 import Adafruit_ADS1x15
 from thermistor import solarthermistor
 
 #Start Connection to Thermocouple ADC
 adc1_conn = Adafruit_ADS1x15.ADS1115()
 
 while True:
-    adc1 = solarthermistor.thermistorDataCollector(adc_conn)
+    adc1 = solarthermistor.thermistorDataCollector(adc1_conn)
     print(adc1.get_data())
